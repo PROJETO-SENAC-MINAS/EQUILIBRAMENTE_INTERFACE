819baa7c1979ab3c4e9fc0547aa467b1fd26b8c7
Guilherme Andrade <129386133+Gu1Andrade@users.noreply.github.com>
2026-01-29 21:14:31 -0300

    validacao-login

---
+++ js/login.js
@@ -0,0 +1,41 @@
+function validarPaciente() {
+    const usuario = document.getElementById("emailPaciente").value;
+    const senha = document.getElementById("senhaPaciente").value;
+    
+
+    const pacienteCorreto = "paciente@gmail.com";
+    const senhaCorreta = "1234";
+
+    if (usuario === pacienteCorreto && senha === senhaCorreta) {
+        alert("Usuario e senha corretos!");
+        localStorage.setItem("usuarioLogado", pacienteCorreto);
+        // window.location.href = "acesso.html";
+    } else {
+        alert("Usuario ou e senha incorreto!");
+    }
+}
+
+function validarPsicologo(event) {
+    event.preventDefault(); 
+
+    const emailPsicologo = document.getElementById("emailPsicologo").value.trim();
+    const crpPsicologo = document.getElementById("crpPsicologo").value.trim();
+    const senhaPsicologo = document.getElementById("senhaPsicologo").value.trim();
+
+    const emailCorreto = "psicologo@gmail.com";
+    const senhaCorreta = "1234";
+    const crpCorreto = "01/1234";
+
+    if (
+        emailPsicologo === emailCorreto &&
+        senhaPsicologo === senhaCorreta &&
+        crpPsicologo === crpCorreto
+    ) {
+        alert("Usuário autenticado com sucesso!");
+        localStorage.setItem("psicologoLogado", emailCorreto);
+
+        
+    } else {
+        alert("Usuário, senha ou CRP incorretos!");
+    }
+}
