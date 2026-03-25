0e3d4f379205993949b3fcadcd2deed1951acebb
Arthur H. <arthurhpb7@icloud.com>
2026-03-25 19:25:59 -0300

    Atualização

--- js/login.js
+++ js/login.js
@@ -1,26 +1,31 @@
-function validarPaciente() {
-    const usuario = document.getElementById("emailPaciente").value;
-    const senha = document.getElementById("senhaPaciente").value;
-    
+function validarPaciente(event) {
+    event.preventDefault();
+
+    const usuario = document.getElementById("emailPaciente").value.trim();
+    const senha = document.getElementById("senhaPaciente").value.trim();
 
     const pacienteCorreto = "paciente@gmail.com";
     const senhaCorreta = "1234";
 
     if (usuario === pacienteCorreto && senha === senhaCorreta) {
+<<<<<<< HEAD
         alert("Usuario e senha corretos!");
         localStorage.setItem("usuarioLogado", pacienteCorreto);
         window.location.href = "consultas.html";
+=======
+        alert("Login realizado!");
+        window.location.href = "menu.html";
+>>>>>>> dce99dc3a8e8a82172e7c7158e57458a2570c0ad
     } else {
-        alert("Usuario ou e senha incorreto!");
+        alert("Usuário ou senha incorretos!");
     }
 }
 
-function validarPsicologo(event) {
-    event.preventDefault(); 
+function validarPsicologo() {
 
-    const emailPsicologo = document.getElementById("emailPsicologo").value.trim();
-    const crpPsicologo = document.getElementById("crpPsicologo").value.trim();
-    const senhaPsicologo = document.getElementById("senhaPsicologo").value.trim();
+    const emailPsicologo = document.getElementById("emailPsicologo").value;
+    const crpPsicologo = document.getElementById("crpPsicologo").value;
+    const senhaPsicologo = document.getElementById("senhaPsicologo").value;
 
     const emailCorreto = "psicologo@gmail.com";
     const senhaCorreta = "1234";
